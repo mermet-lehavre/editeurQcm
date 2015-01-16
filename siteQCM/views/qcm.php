089dfaee533a796f5a0be939d505921992957829
Erwan Lebrun <erwan76.lebrun@gmail.com>
2015-01-16 19:51:28 +0100

    Initialisation du projet Web

---
+++ siteQCM/views/qcm.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>QCM</title>
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap-theme.css">
+</head>
+<body>
+
+
+<script src="../js/bootstrap.js"></script>
+</body>
+</html>
