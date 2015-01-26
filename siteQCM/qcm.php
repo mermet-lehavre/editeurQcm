0b0d1c49454b124ff28b8f4e84a7b93dee059c60
Theblues <erwan76.lebrun@gmail.com>
2015-01-26 15:28:50 +0100

    Ajout de jquery dans le projet

---
+++ siteQCM/qcm.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>QCM</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+</head>
+<body>
+
+<?php echo "test" ?>
+
+<script src="js/jquery-2.1.3.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
